d39dca0e5b5aa75d1c905bffcaf7ec9b8a3acffa
Rusbell Lozano <ruslllozano@gmail.com>
2021-02-11 22:04:54 -0500

    deploy fix

--- src/index.js
+++ src/index.js
@@ -24,6 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-app.listen(3000, ()=>{
+app.listen( process.env.PORT ||3000, ()=>{
     console.log("Server on port", 3000)
 })
